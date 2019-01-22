aed584c25482e734eff893a9d6c21d9077878bbf
Nikita Grigoryev <nikitaqwert5@gmail.com>
2019-01-22 18:22:38 +0100

    fix

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,39 +2,9 @@
 
 namespace App\Presenters;
 
-use App\Model\ArticleRepository;
 use Nette;
-use Nette\Application\UI\Form;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-
-
-    /** @var ArticleRepository */
-    private $articleRepository;
-
-
-   public function __construct(ArticleRepository $arcticleRepository)
-   {
-       $this->articleRepository = $arcticleRepository;
-   }
-
-
-    /** render some templates and set paginator for homepage
-     * @param int $page
-     */
-    public function renderDefault($page = 1)
-    {
-        $newsCount = $this->articleRepository->getNewsCount();
-        $paginator = new Nette\Utils\Paginator;
-        $paginator->setItemCount($newsCount);
-        $paginator->setItemsPerPage(1);
-        $paginator->setPage($page);
-        $news = $this->articleRepository->getNews($paginator->getLength(), $paginator->getOffset());
-
-        $this->template->news = $news;
-        $this->template->paginator = $paginator;
-
-    }
 }
